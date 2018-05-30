9aa8fd021da9346a527b51da53bf7499fb058afc
krl <karolpiekara@gmail.com>
2018-05-30 22:59:29 +0200

    Students grouping added.

--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,18 @@
 class PassedOrFailed
 
+	def initialize(data, threshold)
+		@threshold = threshold.to_i
+		@data = data
+	end
+
+	def perform
+		output = {passed: {}, failed: {}}
+
+		@data.each do |k, v|
+			result = v.to_i >= @threshold ? :passed : :failed
+			output[result][k] = v.to_i
+		end
+
+		return output
+	end
 end
